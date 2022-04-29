6f32aa3b080ca81833030b80c54c3814b38fa152
David Stansby <dstansby@gmail.com>
2022-04-29 13:30:02 +0100

    Add emtpy mpl canvas

--- src/napari_matplotlib/__init__.py
+++ src/napari_matplotlib/__init__.py
@@ -1,11 +1,4 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-
-
-
-
-
-from ._widget import ExampleQWidget, example_magic_widget
--- src/napari_matplotlib/_widget.py
+++ src/napari_matplotlib/_widget.py
@@ -1,13 +1,7 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/plugins/stable/guides.html#widgets
-
-Replace code below according to your needs.
-"""
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+import napari
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
+from qtpy.QtWidgets import QHBoxLayout, QWidget
 
 
 class ExampleQWidget(QWidget):
@@ -15,27 +9,13 @@ class ExampleQWidget(QWidget):
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+    def __init__(self, napari_viewer: napari.viewer.Viewer):
         super().__init__()
         self.viewer = napari_viewer
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        axes = static_canvas.figure.subplots()
 
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+        self.layout().addWidget(static_canvas)
