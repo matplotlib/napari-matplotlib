297998ee3f0c83c95757d17fc242c875050fcb1b
David Stansby <dstansby@gmail.com>
2022-04-29 20:45:23 +0100

    Use pydata theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ main_doc = "index"
 
 # General information about the project.
 project = "napari-matplotlib"
-copyright = "2015, David Stansby"
+copyright = "2022, David Stansby"
 author = "David Stansby"
 
 # The version info for the project you're documenting, acts as replacement for
@@ -107,7 +107,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -136,7 +136,7 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
