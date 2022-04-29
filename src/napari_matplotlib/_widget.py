7d03aa2450271c5628f1f2e3176f440f19d62e8e
David Stansby <dstansby@gmail.com>
2022-04-29 16:57:12 +0100

    Set layer automatically from napari window selection

--- src/napari_matplotlib/_widget.py
+++ src/napari_matplotlib/_widget.py
@@ -2,38 +2,38 @@ import napari
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
 from qtpy.QtWidgets import QComboBox, QVBoxLayout, QWidget
+import numpy as np
 
 
 class ExampleQWidget(QWidget):
     def __init__(self, napari_viewer: napari.viewer.Viewer):
         super().__init__()
         self.viewer = napari_viewer
-
-        self.setLayout(QVBoxLayout())
+        self.layer = self.viewer.layers[0]
 
         self.canvas = FigureCanvas(Figure(figsize=(5, 3)))
         self.axes = self.canvas.figure.subplots()
 
-        self.layer_box = QComboBox()
-        self.layout().addWidget(self.layer_box)
+        self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.canvas)
 
-        self.update_layers()
+        self.viewer.dims.events.current_step.connect(self.hist_current_layer)
+        self.viewer.layers.selection.events.active.connect(self.update_layer)
+
         self.hist_current_layer()
 
-        self.viewer.dims.events.current_step.connect(self.hist_current_layer)
-
-    def update_layers(self):
-        self.layer_box.clear()
-        names = [layer.name for layer in self.viewer.layers]
-        self.layer_box.insertItems(0, names)
+    def update_layer(self, event):
+        # Update current layer when selection changed in viewer
+        if event.value:
+            self.layer = event.value
+            self.hist_current_layer()
 
     def hist_current_layer(self):
         self.axes.clear()
-        layer_name = self.layer_box.currentText()
-        layer = self.viewer.layers[layer_name]
+        layer = self.layer
         z = self.viewer.dims.current_step[0]
+        bins = np.linspace(np.min(layer.data), np.max(layer.data), 100)
         data = layer.data[z]
-        self.axes.hist(data.ravel(), bins="auto")
-        self.axes.set_title(f"{layer_name}, z={z}")
+        self.axes.hist(data.ravel(), bins=bins)
+        self.axes.set_title(f"{layer.name}, z={z}")
         self.canvas.draw()
