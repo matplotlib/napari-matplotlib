7e11d4e9f36b5a9cfaa5fa94882c45e3354f7ffa
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-05 06:09:19 +0200

    Removed separate MPL Canvas class

--- src/napari_matplotlib/base.py
+++ src/napari_matplotlib/base.py
@@ -5,27 +5,6 @@ from qtpy.QtWidgets import QVBoxLayout, QWidget
 
 __all__ = ["NapariMPLWidget"]
 
-class MplCanvas(FigureCanvas):
-    """
-    Defines the canvas of the matplotlib window
-    From https://github.com/haesleinhuepf/napari-workflow-inspector/blob/main/src/napari_workflow_inspector/_dock_widget.py
-    """
-    def __init__(self):
-        self.fig = Figure()                         # create figure
-        self.axes = self.fig.add_subplot(111)       # create subplot
-
-        self.axes.spines['bottom'].set_color('white')
-        self.axes.spines['top'].set_color('white')
-        self.axes.spines['left'].set_color('white')
-        self.axes.spines['right'].set_color('white')
-        self.fig.patch.set_facecolor('#262930')
-        self.axes.set_facecolor('#262930')
-        self.axes.grid(which='major', linestyle='--', color='white', alpha=0.6)
-        self.axes.tick_params(axis='both', colors='white')
-
-        FigureCanvas.__init__(self, self.fig)       # initialize canvas
-        FigureCanvas.updateGeometry(self)
-
 
 class NapariMPLWidget(QWidget):
     """
@@ -52,9 +31,10 @@ class NapariMPLWidget(QWidget):
 
         self.viewer = napari_viewer
         self.figure = Figure(figsize=(5, 3), tight_layout=True)
-        self.canvas = MplCanvas()
+        self.canvas = FigureCanvas()
+        self.canvas.figure.patch.set_facecolor('#262930')
         self.toolbar = NavigationToolbar2QT(self.canvas, self)
-        self.axes = self.canvas.axes
+        self.axes = self.canvas.figure.subplots()
 
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.toolbar)
