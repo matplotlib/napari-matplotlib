6009919636f7bd3c76fba846d3860622469c82e4
ruaridhg <ruaridhg@users.noreply.github.com>
2023-05-09 10:44:26 +0100

    add pytest decorator to test_histogram

--- src/napari_matplotlib/tests/test_histogram.py
+++ src/napari_matplotlib/tests/test_histogram.py
@@ -1,8 +1,12 @@
+import pytest
+
 from napari_matplotlib import HistogramWidget
 
 
+@pytest.mark.mpl_image_compare
 def test_example_q_widget(make_napari_viewer, image_data):
     # Smoke test adding a histogram widget
     viewer = make_napari_viewer()
     viewer.add_image(image_data[0], **image_data[1])
-    HistogramWidget(viewer)
+    fig = HistogramWidget(viewer).figure
+    return fig
