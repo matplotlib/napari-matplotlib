68a70ee3222f991b4681ca2237d8e1b9f08aca6a
ruaridhg <ruaridhg@users.noreply.github.com>
2023-05-09 11:19:28 +0100

    matplotlib pytest working for histogram

--- src/napari_matplotlib/tests/test_histogram.py
+++ src/napari_matplotlib/tests/test_histogram.py
@@ -4,9 +4,9 @@ from napari_matplotlib import HistogramWidget
 
 
 @pytest.mark.mpl_image_compare
-def test_example_q_widget(make_napari_viewer, image_data):
+def test_example_q_widget(make_napari_viewer, astronaut_data):
     # Smoke test adding a histogram widget
     viewer = make_napari_viewer()
-    viewer.add_image(image_data[0], **image_data[1])
+    viewer.add_image(astronaut_data[0], **astronaut_data[1])
     fig = HistogramWidget(viewer).figure
     return fig
