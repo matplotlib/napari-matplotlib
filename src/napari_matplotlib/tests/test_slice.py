bf815637be7b4fb4cba7c9f3466b0b12cebb88dd
ruaridhg <ruaridhg@users.noreply.github.com>
2023-05-15 16:43:57 +0100

    Added baseline test_slice image, removed comments and reverted gitignore for baseline folder

--- src/napari_matplotlib/tests/test_slice.py
+++ src/napari_matplotlib/tests/test_slice.py
@@ -5,10 +5,7 @@ from napari_matplotlib import SliceWidget
 
 @pytest.mark.mpl_image_compare
 def test_slice(make_napari_viewer, brain_data):
-    # Smoke test adding a slice widget
     viewer = make_napari_viewer()
-    viewer.add_image(
-        brain_data[0], **brain_data[1]
-    )  # (np.random.random((100, 100, 100)))
+    viewer.add_image(brain_data[0], **brain_data[1])
     fig = SliceWidget(viewer).figure
     return fig
