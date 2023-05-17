e594799cbba821c74aace25e55e80991df5873bf
ruaridhg <ruaridhg@users.noreply.github.com>
2023-05-17 16:20:05 +0100

    Added 3D test for test_histogram and 2D and 3D test images

--- src/napari_matplotlib/tests/test_histogram.py
+++ src/napari_matplotlib/tests/test_histogram.py
@@ -6,10 +6,21 @@ from napari_matplotlib import HistogramWidget
 
 
 @pytest.mark.mpl_image_compare
-def test_example_q_widget(make_napari_viewer, astronaut_data):
+def test_histogram_2D(make_napari_viewer, astronaut_data):
     # Smoke test adding a histogram widget
     viewer = make_napari_viewer()
     viewer.add_image(astronaut_data[0], **astronaut_data[1])
+    fig = HistogramWidget(viewer).figure
+    # Need to return a copy, as original figure is too eagerley garbage
+    # collected by the widget
+    return deepcopy(fig)
+
+
+@pytest.mark.mpl_image_compare
+def test_histogram_3D(make_napari_viewer, brain_data):
+    # Smoke test adding a histogram widget
+    viewer = make_napari_viewer()
+    viewer.add_image(brain_data[0], **brain_data[1])
     fig = HistogramWidget(viewer).figure
     # Need to return a copy, as original figure is too eagerley garbage
     # collected by the widget
