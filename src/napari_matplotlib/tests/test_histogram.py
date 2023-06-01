014e34f1cebc9c3d755b65bdce98fafe33876d24
David Stansby <dstansby@gmail.com>
2023-06-01 13:49:45 +0100

    Remove old smoke test comments

--- src/napari_matplotlib/tests/test_histogram.py
+++ src/napari_matplotlib/tests/test_histogram.py
@@ -7,7 +7,6 @@ from napari_matplotlib import HistogramWidget
 
 @pytest.mark.mpl_image_compare
 def test_histogram_2D(make_napari_viewer, astronaut_data):
-    # Smoke test adding a histogram widget
     viewer = make_napari_viewer()
     viewer.add_image(astronaut_data[0], **astronaut_data[1])
     fig = HistogramWidget(viewer).figure
@@ -18,7 +17,6 @@ def test_histogram_2D(make_napari_viewer, astronaut_data):
 
 @pytest.mark.mpl_image_compare
 def test_histogram_3D(make_napari_viewer, brain_data):
-    # Smoke test adding a histogram widget
     viewer = make_napari_viewer()
     viewer.add_image(brain_data[0], **brain_data[1])
     fig = HistogramWidget(viewer).figure
