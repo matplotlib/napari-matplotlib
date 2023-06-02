b2e91fa2ae66f38dfca6050dbbbe30dc30967297
David Stansby <dstansby@gmail.com>
2023-06-02 12:04:06 +0100

    Check that single layer widget plots change w/ layer change

--- src/napari_matplotlib/tests/test_histogram.py
+++ src/napari_matplotlib/tests/test_histogram.py
@@ -3,10 +3,6 @@ from copy import deepcopy
 import pytest
 
 from napari_matplotlib import HistogramWidget
-from napari_matplotlib.tests.helpers import (
-    assert_figures_equal,
-    assert_figures_not_equal,
-)
 
 
 @pytest.mark.mpl_image_compare
@@ -27,26 +23,3 @@ def test_histogram_3D(make_napari_viewer, brain_data):
     # Need to return a copy, as original figure is too eagerley garbage
     # collected by the widget
     return deepcopy(fig)
-
-
-def test_change_layer(make_napari_viewer, brain_data, astronaut_data):
-    viewer = make_napari_viewer()
-    widget = HistogramWidget(viewer)
-
-    viewer.add_image(brain_data[0], **brain_data[1])
-    viewer.add_image(astronaut_data[0], **astronaut_data[1])
-
-    # Select first layer
-    viewer.layers.selection.clear()
-    viewer.layers.selection.add(viewer.layers[0])
-    fig1 = deepcopy(widget.figure)
-
-    # Re-selecting first layer should produce identical plot
-    viewer.layers.selection.clear()
-    viewer.layers.selection.add(viewer.layers[0])
-    assert_figures_equal(widget.figure, fig1)
-
-    # Plotting the second layer should produce a different plot
-    viewer.layers.selection.clear()
-    viewer.layers.selection.add(viewer.layers[1])
-    assert_figures_not_equal(widget.figure, fig1)
