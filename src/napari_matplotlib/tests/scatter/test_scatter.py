360f8f17f8d7f15c2882642b685eaf29290f9791
ruaridhg <32329546+ruaridhg@users.noreply.github.com>
2023-06-02 15:18:51 +0100

    Update src/napari_matplotlib/tests/scatter/test_scatter.py
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- src/napari_matplotlib/tests/scatter/test_scatter.py
+++ src/napari_matplotlib/tests/scatter/test_scatter.py
@@ -39,7 +39,6 @@ def test_scatter_3D(make_napari_viewer, brain_data):
     )
     # De-select existing selection
     viewer.layers.selection.clear()
-    # pdb.set_trace()
     axis = viewer.dims.last_used
     slice_no = array(brain_data[0]).shape[0] - 1
     viewer.dims.set_current_step(axis, slice_no)
