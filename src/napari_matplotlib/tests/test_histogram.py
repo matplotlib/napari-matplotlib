21b5823f9cb645044ef0bb64c08030a015ec8f17
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-06-02 12:11:59 +0100

    Resolves #96.
    Not a nice global fix, but by setting the test fixture viewer's theme to
    light, napari-matplotlib will give axes dark grey axes, labels, and ticks.

--- src/napari_matplotlib/tests/test_histogram.py
+++ src/napari_matplotlib/tests/test_histogram.py
@@ -8,6 +8,7 @@ from napari_matplotlib import HistogramWidget
 @pytest.mark.mpl_image_compare
 def test_histogram_2D(make_napari_viewer, astronaut_data):
     viewer = make_napari_viewer()
+    viewer.theme = "light"
     viewer.add_image(astronaut_data[0], **astronaut_data[1])
     fig = HistogramWidget(viewer).figure
     # Need to return a copy, as original figure is too eagerley garbage
@@ -18,6 +19,7 @@ def test_histogram_2D(make_napari_viewer, astronaut_data):
 @pytest.mark.mpl_image_compare
 def test_histogram_3D(make_napari_viewer, brain_data):
     viewer = make_napari_viewer()
+    viewer.theme = "light"
     viewer.add_image(brain_data[0], **brain_data[1])
     axis = viewer.dims.last_used
     slice_no = brain_data[0].shape[0] - 1
