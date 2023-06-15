6f75f44221ce62400f41fbe5476a494834fc5449
dstansby <dstansby@users.noreply.github.com>
2023-06-15 12:49:52 +0000

    Deploying to gh-pages from @ matplotlib/napari-matplotlib@e191c482a393c2e484e643ccaa5ce12923387018 🚀

--- src/napari_matplotlib/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
-
-
-from .histogram import *  # NoQA
-from .scatter import *  # NoQA
-from .slice import *  # NoQA
