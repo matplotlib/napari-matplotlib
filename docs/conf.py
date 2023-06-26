bdd3172c2ef0d690599ae0ca9e30b71e47a5878f
David Stansby <dstansby@gmail.com>
2023-06-26 10:42:11 +0100

    Minor improvements to docs

--- docs/conf.py
+++ docs/conf.py
@@ -92,3 +92,17 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/matplotlib/napari-matplotlib",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+        }
+    ],
+    "logo": {
+        "text": "napari-matplotlib",
+    },
+}
