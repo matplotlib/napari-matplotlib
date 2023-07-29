460721381ac0b244531bd2ff84ef1d0059eb8404
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-07-29 10:00:58 +0200

    revert changes in base

--- src/napari_matplotlib/base.py
+++ src/napari_matplotlib/base.py
@@ -209,6 +209,7 @@ class NapariMPLWidget(BaseNapariMPLWidget):
         """
         Current z-step of the napari viewer.
         """
+        return self.viewer.dims.current_step[0]
         if self.viewer.dims.ndim < 3:
             return slice(None)
         return self.viewer.dims.current_step[-3]
