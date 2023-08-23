8ac1cd2c68118123017135622f54bc443ea032b8
David Stansby <dstansby@gmail.com>
2023-08-23 14:47:42 +0100

    Fix data dimensions

--- src/napari_matplotlib/tests/test_slice.py
+++ src/napari_matplotlib/tests/test_slice.py
@@ -11,11 +11,11 @@ def test_slice_3D(make_napari_viewer, brain_data):
     viewer.theme = "light"
 
     data = brain_data[0]
-    assert data.ndim == 3
-    viewer.add_image(brain_data[0], **brain_data[1])
+    assert data.ndim == 3, data.shape
+    viewer.add_image(data, **brain_data[1])
 
     axis = viewer.dims.last_used
-    slice_no = brain_data[0].shape[0] - 1
+    slice_no = data.shape[0] - 1
     viewer.dims.set_current_step(axis, slice_no)
     fig = SliceWidget(viewer).figure
     # Need to return a copy, as original figure is too eagerley garbage
@@ -28,9 +28,10 @@ def test_slice_2D(make_napari_viewer, astronaut_data):
     viewer = make_napari_viewer()
     viewer.theme = "light"
 
-    data = astronaut_data[0]
-    assert data.ndim == 2
-    viewer.add_image(astronaut_data[0], **astronaut_data[1])
+    # Take first RGB channel
+    data = astronaut_data[0][:, :, 0]
+    assert data.ndim == 2, data.shape
+    viewer.add_image(data, **astronaut_data[1])
 
     fig = SliceWidget(viewer).figure
     # Need to return a copy, as original figure is too eagerley garbage
